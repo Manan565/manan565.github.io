695b16621b87a13baa221e773b068f16d76d5417
Manan565 <mananmehta634@gmail.com>
2025-02-06 02:58:37 -0800

    created the contact template

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div>
+        <h1>Let's Connect</h1>
+        <p>
+          I'm always open to new opportunities and collaborations. Whether you
+          have an exciting project in mind, want to discuss potential job
+          opportunities, or simply connect, I'd love to hear from you.
+        </p>
+      </div>
+
+      <div>
+        <p>THISSSSSS SI SSS COOOOOOOLSSDLLDLD</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
