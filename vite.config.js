0405fab087706a4c3c180a19c51399a94ea50f59
Manan565 <mananmehta634@gmail.com>
2025-02-11 23:22:43 -0800

    .

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/manan565.github.io/",
-  build: {
-    outDir: "dist",
-  },
+  base: "https://manan565.github.io/",
 });
